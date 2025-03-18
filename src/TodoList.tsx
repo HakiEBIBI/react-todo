e4ddf14771266691d5075e4c4bbd81cc356ca44b
HakiEBIBI <haki.ebibi@jobtrek.ch>
2025-03-18 15:47:20 +0100

    feat: added a error handling close: #6

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -2,14 +2,18 @@ import {Todo} from "./TodoInterface.ts"
 import {ListTodo} from "./ListTodo.tsx";
 
 
-export const TodoList = ({todoList, deleteTodo}: { todoList: Todo[], deleteTodo: (todo: Todo) => void }) => {
+export const TodoList = ({todoList, deleteTodo, setError}: {
+    todoList: Todo[],
+    deleteTodo: (todo: Todo) => void,
+    setError: (error: string | null) => void,
+}) => {
     return (
         <div className="all-list">
             <div className="todo-list">
                 <ul>
                     {todoList.map((todo) => (
                         <ListTodo todo={todo} deleteTodo={deleteTodo} date={todo.due_date} title={todo.title}
-                                  key={todo.id}/>))}
+                                  key={todo.id} setError={setError}/>))}
                 </ul>
             </div>
         </div>
